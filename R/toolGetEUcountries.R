9017ba2d7f87d8f6bc3fb0ccb74fb88ee14d0625
Johannes Koch <jokoch@pik-potsdam.de>
2023-07-13 13:30:02 +0200

    Fix bug in toolGetMapping

--- R/toolGetEUcountries.R
+++ R/toolGetEUcountries.R
@@ -1,7 +1,7 @@
 # These countries are the ones (essentiall the EU-27) that receive special treatment
 # in the SSP2EU scenario.
 toolGetEUcountries <- function(onlyWithARIADNEgdpData = FALSE) {
-  x <- toolGetMapping("regionmappingH12.csv", where = "mappingfolder") %>%
+  x <- toolGetMapping("regionmappingH12.csv", type = "regional", where = "mappingfolder") %>%
     tibble::as_tibble() %>%
     dplyr::filter(.data$RegionCode == "EUR", .data$CountryCode != "GBR") %>%
     dplyr::pull(.data$CountryCode)
