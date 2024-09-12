15fb8d056bb67f43efe6bf77b16b1c1b1e46b0af
Johannes Koch <jokoch@pik-potsdam.de>
2024-09-12 11:29:42 +0200

    Refactor and change default scenarios
    Replace SSP2EU with a placeholder, as it no longer a desired scenario, but required in the input preparation pipeline. Remove completely in next release.
    Add ADB sources and scenarios (not returned by default yet).
    Urban pop share scenarios are improved, so as to never exceed 1, even with bezier extensions.
    SSP labour scenarios are now also filled in with UN_popDiv data.
    SSP population scenarios now use short term growth rates from PEAP-UN_PopDiv, instead of PEAP-MI, improving the shorterm projections of the smaller countries.
    Refactor function arguments that did not comply with lintr rules (fixes #13).
    Mark "scenario.indicator" naming option as deprecated and remove in next release.
    Fix all linter warnings.
    Improve scenario documentation and references in vignette (fixes #56 and fixes #57).
    Improve and simplify documentation (fixes #16).
    Remove "unit" from most functions, assuming instead that the default dollar unit is used throughout the package.
    Clean up use of James data set for filling and backwards extension to 1960. These two functions are now cleanly separated from each other. Also, use of the James2019 dataset is dropped (fixes #62), due to the fact that there is no reference for this dataset. This does not affect the scenarios much since the dataset is mainly used for its growth rates, and they shouldn't differ to much.
    Refactor toolReduce and toolFillWith. toolFillWith now writes to the log which countries are filled in.
    Export some of the more useful tool functions.
    Speed up bezier extension (fixes #78).
    Remove MI from future datasets and make it clear that we assume constant extrapolation for most of the MI countries.
    Fixes #52.

--- R/toolCheckUserInput.R
+++ R/toolCheckUserInput.R
@@ -1,51 +1,57 @@
-toolCheckUserInput <- function(driver, args) {
-  permitted_args <- c("pastData", "futureData", "harmonization", "scenario", "unit", "yearEnd", "extension2150",
-                      "average2020", "naming", "popAsWeight", "asShare", "GDPPast", "GDPFuture", "GDPpcPast", "GDPpcFuture",
-                      "PopulationPast", "PopulationFuture", "LabourPast", "LabourFuture", "UrbanPast", "UrbanFuture")
-  if (!all(names(args) %in% permitted_args)) {
-    stop(glue("Bad argument to calc{driver}: '{names(args)[! names(args) %in% permitted_args]}'."))
+#' Check user input
+#'
+#' toolCheckUserInput checks the user input.
+#'
+#' @param args A list with all the arguments passed to the parent function
+#' @inheritParams calcDriver
+#' @keywords internal
+#' @return TRUE
+toolCheckUserInput <- function(driver, args) { # nolint: cyclocomp_linter.
+  permittedArgs <- c("scenario", "unit", "extension2150", "extension1960", "average2020", "naming", "popAsWeight",
+                     "asShare", "pastData", "futureData", "harmonization")
+  if (!all(names(args) %in% permittedArgs)) {
+    stop(glue("Bad argument to calc{driver}: '{names(args)[! names(args) %in% permittedArgs]}'."))
   }
-
-  # Check existence of pastData, futureData and harmonization
-  if (!all(c("pastData", "futureData", "harmonization") %in% names(args)) &&
-       any(c("pastData", "futureData", "harmonization") %in% names(args))) {
-     stop("If you intend to overide the scenario argument, you must set pastData, futureData and hamrmonization.")
-  }
-  overrideScen <- if (all(c("pastData", "futureData", "harmonization") %in% names(args))) TRUE else FALSE
 
   # Check population scenario availability for any GDPpc scenario
-  if (!overrideScen && driver == "GDPpc" &&
-      !all(args$scenario %in% toolGetScenarioDefinition("Population")$scenario)) {
+  if (driver == "GDPpc" && !all(args$scenario %in% toolGetScenarioDefinition("Population")$scenario)) {
     stop("GDPpc scenarios require equivalent population scenarios to use as weight.")
   }
 
   # Check 'extension2150' argument
   if ("extension2150" %in% names(args) && !args$extension2150 %in% c("none", "bezier", "constant")) {
-     stop(glue("Bad argument to calc{driver}. 'extension2150' has to be either 'none', 'bezier' or 'constant', \\
+    stop(glue("Bad argument to calc{driver}. 'extension2150' has to be either 'none', 'bezier' or 'constant', \\
                not '{args$extension2150}'."))
+  }
+
+  # Check 'extension1960' argument
+  if ("extension1960" %in% names(args) && !args$extension1960 %in% c("none", "MI-James", "MI", "James")) {
+    stop(glue("Bad argument to calc{driver}. 'extension1960' has to be either 'none', 'MI', 'James', or a \\
+              combination of both, e.g. 'MI-James', not '{args$extension2150}'."))
   }
 
   # Check 'naming' argument
   if ("naming" %in% names(args) && !args$naming %in% c("indicator_scenario", "indicator.scenario", "scenario")) {
-     stop(glue("Bad argument to calc{driver}. 'naming' has to be either 'indicator_scenario', 'indicator.scenario' \\
-                or 'scenario', not '{args$naming}'."))
+    stop(glue("Bad argument to calc{driver}. 'naming' has to be either 'indicator_scenario', 'indicator.scenario' \\
+              or 'scenario', not '{args$naming}'."))
   }
 
-  # Check 'unit' argument
-  if ("unit" %in% names(args) && (length(args$unit) != 1 || !grepl("^constant (2017|2005) ", args$unit))) {
-     stop(glue("Bad argument to calc{driver}. Currently, only constant 2017 dollars are accepted."))
+  # Check 'unit' argument. Only constant dollars are allowed.
+  if ("unit" %in% names(args) && (length(args$unit) != 1 || !grepl("^constant ", args$unit))) {
+    stop(glue("Bad argument to calc{driver}. Only constant dollars are accepted."))
   }
 
   # Check 'average2020' argument
   if ("average2020" %in% names(args) && (length(args$average2020) != 1 || !is.logical(args$average2020))) {
-     stop(glue("Bad argument to calc{driver}. 'average2020' has to be TRUE of FALSE."))
+    stop(glue("Bad argument to calc{driver}. 'average2020' has to be TRUE of FALSE."))
   }
 
   # Check parallel map-reduce compatibility
   if (any(purrr::map_lgl(args,
                          ~ !is.null(.x) &&
-                         length(.x) != 1 &&
-                         length(.x) != max(purrr::map_dbl(args, length))))) {
+                           length(.x) != 1 &&
+                           length(.x) != max(purrr::map_dbl(args, length))))) {
     stop(glue("Arguments to calc{driver} need to be either length 1 or equal to the length of the longest argument."))
   }
+  invisible(TRUE)
 }
--- R/toolCompareProxyMagpie.R
+++ R/toolCompareProxyMagpie.R
@@ -1,6 +1,15 @@
-compare_proxy.magpie <- function(x, path = "x") { # nolint
+#' Magpie proxy for waldo::compare
+#'
+#' Magpie proxy for waldo::compare
+#'
+#' @param x x
+#' @param path "x"
+#' @keywords internal
+#' @return list
+compare_proxy.magpie <- function(x, path = "x") { # nolint: object_name_linter.
   if (any(grepl("^ (origin:|creation date:)", comment(x)))) {
     comment(x) <- comment(x)[!grepl("^ (origin:|creation date:)", comment(x))]
   }
+
   list(object = as.vector(x), path = paste0("as.vector(", path, ")"))
 }
